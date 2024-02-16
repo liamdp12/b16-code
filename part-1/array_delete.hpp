2303c3f5475a8abce8c29ae6523f3988d4145d8f
Liam Perreault <liamperreault12@gmail.com>
2024-02-16 11:35:37 +0000

    work in progress on array_delete

--- part-1/array_delete.hpp
+++ part-1/array_delete.hpp
@@ -4,11 +4,31 @@
 #include <cstddef>
 #include <utility>
 #include <vector>
+#include <iostream>
 
 template <typename T>
 void array_delete(std::vector<T> &A, std::size_t index)
 {
     // WRITE YOUR CODE HERE
+    assert(index<A.size());
+    std::cout << "index: " << index << std::endl;
+    if (index == (A.size() - 1)) {
+        A.pop_back();
+    } else {
+        auto i = A.size() - 1;
+        T last = A[i];
+        T temp;
+        A.pop_back();
+        i = i - 1;
+        while (i >= index) {
+            std::cout << "i: " << i << std::endl;
+            temp = A[i]; // starts at second to last element
+            A[i] = last;
+            last = temp;
+            i--;
+        }
+
+    }
 }
 
 #endif // __array_delete__
